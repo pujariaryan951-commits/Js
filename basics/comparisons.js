f7b13f19e2bebe2ea86088e43bc25fec75a43fa9
pujariaryan951-commits <pujariaryan951@gmail.com>
2025-10-27 13:58:06 +0000

    comparisons

---
+++ basics/comparisons.js
@@ -0,0 +1,6 @@
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0); // unpredicted output....so must be avoided
+
+console.log("2" == 1); //automatically converts and compares
+console.log("2" === 1);//"===" also checks the datatype before comparison
