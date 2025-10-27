1a58396fd73c6cf5aaf214081fff2cb2434ac3d2
pujariaryan951-commits <pujariaryan951@gmail.com>
2025-10-27 12:55:25 +0000

    datatypes and conversions

---
+++ basics/dataConversion.js
@@ -0,0 +1,16 @@
+//data conversion
+let isLoggedIn = ("Aryan")
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//for boolean
+/*
+1 => true ; 0 => false
+"" => false ; "Aryan" => true
+*/
+
+//Numbers converted to strings looks same while printing but datatype changes
+let Num = 33
+let stringNum = String(Num)
+console.log(stringNum);
+console.log(typeof stringNum); //33 is now a string
+
--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,10 +1,11 @@
-let name = "Aryan" //string
-let age = 20 //number
+let name = "Aryan" // string
+let age = 20 // number
 let isLoggedIn = true //boolean
 // null => standalone value
 // undefined => value not given
 // symbol => unique 
 // object =>
 
-console.log(typeof null); //object
-    
+console.log(typeof null); // object
+
+
