7421e77e241de35f6d40a9c96b5af771d15f21d3
pujariaryan951-commits <pujariaryan951@gmail.com>
2025-10-27 13:41:43 +0000

    operations

---
+++ basics/Operations.js
@@ -0,0 +1,19 @@
+// ************* OPERATIONS ***************
+
+let value = 3
+let negValue = -value
+console.log(value)
+console.log(negValue);
+
+// console.log(2+2); //Addition
+// console.log(2-2); //Subtraction
+// console.log(2*2); //Multiplication
+// console.log(2**2); //Power
+// console.log(2/2); //Division
+// console.log(2%2); //Modular
+
+let str1 = "Aryan"
+let str2 = " Pujari"
+
+let str3 = str1 + str2
+console.log(str3);
