4d9409896b0a84dbd743bd6dfcda1babc08badc3
pujariaryan951-commits <pujariaryan951@gmail.com>
2025-10-27 11:59:08 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553 
+//constant 
+let accountEmail = "aryan@gmail.com"
+// used for variable
+var accountPass = "12345"
+//not recommended
+accountCity = "Udupi" 
+//can be done but not recommended
+
+accountEmail = "Ap@gmail.com"
+accountPass = "21345"
+accountCity = "Mysore"
+
+console.table([accountEmail ,accountId, accountPass, accountCity])
