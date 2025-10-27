2bba2852a5e88081e98e6311b6f8c59dc6999634
pujariaryan951-commits <pujariaryan951@gmail.com>
2025-10-27 11:58:50 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+let name = "Aryan" //string
+let age = 20 //number
+let isLoggedIn = true //boolean
+// null => standalone value
+// undefined => value not given
+// symbol => unique 
+// object =>
+
+console.log(typeof null); //object
+    
